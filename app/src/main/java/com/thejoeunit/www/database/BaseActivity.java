34ac94ecc18380d254cc340c562d330e5c3525d9
WhanDooS2 <cjh19890212@gmail.com>
2017-09-11 15:02:57 +0900

    Initial commit

---
+++ app/src/main/java/com/thejoeunit/www/database/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.thejoeunit.www.database;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by the on 2017-09-11.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+    public abstract void bindViews();
+}
